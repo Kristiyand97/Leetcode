448398be257001d3575780d1297197a13e859b9d
Kristiyan Dimov <kristiyand97@gmail.com>
2024-07-03 17:09:16 +0300

    Time: 37 ms (40.95%) | Memory: 11.6 MB (49.39%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def restoreString(self, s, indices):
+
+        result = [""] * len(s)
+
+        for i, index in enumerate(indices):
+            result[index] = s[i]
+
+        return "".join(result)
+
+
